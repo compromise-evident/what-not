9e9916487bca6e97af617e80fc8c717c43aa95d3
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2026-07-03 15:41:39 -0600

    Add files via upload

--- ref.cpp
+++ ref.cpp
@@ -15,8 +15,7 @@ int main()
 	
 	//Gets path.
 	std::cout << "\nDrop/enter file:\n";
-	std::string path; std::getline(std::cin, path);
-	if(path[0] == '\'') {path.erase(0, 1); path.pop_back(); path.pop_back();} //If drag-n-dropped.
+	std::string path; std::getline(std::cin, path); if(path[0] == '\'') {path.erase(0, 1); path.pop_back(); path.pop_back();}
 	if(!std::filesystem::exists(path)) {std::cout << "\nNo path " << path << "\n"; return 1;}
 	
 	//Checks if file is empty.
