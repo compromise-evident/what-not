0efea5292e5be705f5116a0860e224ab4ccd7bf9
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2026-07-02 19:18:01 -0600

    Add files via upload

---
+++ see_bits_and_bit_occurrence_of_a_hash.cpp
@@ -0,0 +1,41 @@
+//YOUR CONTROLS:                                                                                    Run it: "apt install g++ geany". Open the .cpp in Geany. Hit F9 once. F5 to run.
+char your_hash[] = {"6a74cb02b60a820f33139bd65c17861daf6f0707efcd5e0a03808317be46488e"};
+//Enter SHA-256, SHA-512, or any string or hash containing symbols "0123456789abcdef".
+
+
+
+#include <iostream>
+int main()
+{	std::string bits;
+	for(long long a = 0; your_hash[a] != '\0'; a++)
+	{	if     (your_hash[a] == '0') {bits += "0000";}
+		else if(your_hash[a] == '1') {bits += "0001";}
+		else if(your_hash[a] == '2') {bits += "0010";}
+		else if(your_hash[a] == '3') {bits += "0011";}
+		else if(your_hash[a] == '4') {bits += "0100";}
+		else if(your_hash[a] == '5') {bits += "0101";}
+		else if(your_hash[a] == '6') {bits += "0110";}
+		else if(your_hash[a] == '7') {bits += "0111";}
+		else if(your_hash[a] == '8') {bits += "1000";}
+		else if(your_hash[a] == '9') {bits += "1001";}
+		else if(your_hash[a] == 'a') {bits += "1010";}
+		else if(your_hash[a] == 'b') {bits += "1011";}
+		else if(your_hash[a] == 'c') {bits += "1100";}
+		else if(your_hash[a] == 'd') {bits += "1101";}
+		else if(your_hash[a] == 'e') {bits += "1110";}
+		else if(your_hash[a] == 'f') {bits += "1111";}
+		else                         {std::cout << "\nError: hash contains symbols other than \"0123456789abcdef\".\n"; return 0;}
+	}
+	
+	long long zeros = 0, ones = 0;
+	for(long long a = 0; bits[a] != '\0'; a++)
+	{	if(bits[a] == '0') {zeros++;}
+		else               {ones ++;}
+	}
+	
+	std::cout << " zero bits: " <<  zeros << "\n"
+	          << "  one bits: " <<  ones  << "\n"
+	          << "total bits: " << (zeros + ones) << "\n\n"
+	
+	          << "the bits:\n" << bits << "\n";
+}
