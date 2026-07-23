05793fcfa7ada890f35d3649bcd1c147f2a3e780
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2026-07-22 22:58:53 -0600

    Add files via upload

--- presses_enter_after_you_type_n_characters.cpp
+++ presses_enter_after_you_type_n_characters.cpp
@@ -5,21 +5,32 @@
 #include <termios.h>
 #include <unistd.h>
 int main()
-{	struct termios old_settings, new_settings;
+{	//Automatic enter stuff.
+	struct termios old_settings, new_settings;
 	tcgetattr(STDIN_FILENO, &old_settings);
 	new_settings = old_settings;
-	std::string typed_characters;
 	
+	//Makes keyboard input be accepted automatically, as if pressing enter.
+	new_settings.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_settings);
+	
+	//Begins.
+	std::string typed_characters;
 	for(;;)
 	{	//Reads 5 characters.
 		std::cout << "\nType 5 characters. \"Enter\" will be pressed automatically: ";
 		typed_characters = "";
-		new_settings.c_lflag &= ~(ICANON | ECHO);
-		tcsetattr(STDIN_FILENO, TCSANOW, &new_settings);
 		for(int a = 0; a < 5; a++)
-		{	char c; if(std::cin.get(c)) {typed_characters += c; std::cout << c;}
+		{	char c; if(std::cin.get(c)) {std::cout << c; typed_characters += c;}
 		}
-		tcsetattr(STDIN_FILENO, TCSANOW, &old_settings);
 		std::cout << "\nCaptured string: " << typed_characters << "\n";
 	}
+	
+	//Restores normal keyboard input mode.
+	tcsetattr(STDIN_FILENO, TCSANOW, &old_settings);
 }
+
+//Technically, enter is pressed after each character input,
+//and printing \n makes it look like enter was pressed after 5.
+//And technically technically, that's a total lie. But this way,
+//you can at least understand what the program is trying to do.
