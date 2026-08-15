2a6a5eeae84552d3878e61cbd3732d82caa9d5ab
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2026-08-14 23:04:48 -0600

    Add files via upload

--- mod_sequence.cpp
+++ mod_sequence.cpp
@@ -1,18 +1,19 @@
-//YOUR CONTROLS:                                                                Run it: "apt install g++ geany". Open the .cpp in Geany. Hit F9 once. F5 to run.
+//YOUR CONTROLS:
 unsigned long long    dividend = 1000000000000000000;
 unsigned long long max_divisor = 100000000;
 unsigned long long    quantity = 100000;
 
 
 
+//Run it: "apt install g++ geany". Open the .cpp in Geany. Hit F9 once. F5 to run.
+
 #include <iostream>
-using namespace std;
 int main()
 {	unsigned long long wanted_remainder = 0;
 	for(unsigned long long a = 0; a < quantity; a++, wanted_remainder++)
 	{	for(unsigned long long divisor = 1;; divisor++)
-		{	if((dividend % divisor) == wanted_remainder) {cout << wanted_remainder << " = " << dividend << " mod " << divisor << "\n"; break;}
-			if (divisor  > max_divisor)                  {cout << wanted_remainder << " = " << dividend << " mod >max_divisor\n";      break;}
+		{	if((dividend % divisor) == wanted_remainder) {std::cout << wanted_remainder << " = " << dividend << " mod " << divisor << "\n"; break;}
+			if (divisor  > max_divisor)                  {std::cout << wanted_remainder << " = " << dividend << " mod >max_divisor\n";      break;}
 		}
 	}
 }
